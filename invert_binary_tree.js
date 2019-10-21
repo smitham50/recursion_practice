849ba668b6c10601da2bdb1f4499a3d385782fb0
Aaron Smith <smitham50@gmail.com>
2019-10-21 07:45:23 -0400

    added search function for binary search tree

--- invert_binary_tree.js
+++ invert_binary_tree.js
@@ -12,31 +12,23 @@ class Node {
   }
 }
 
-let tree = new BinaryTree()
-let n1 = new Node(5);
-let n2 = new Node(11);
+let searchTree = new BinaryTree()
+let n1 = new Node(15);
+let n2 = new Node(10);
 let n3 = new Node(20);
-let n4 = new Node(7);
-let n5 = new Node(4);
-let n6 = new Node(35);
-let n7 = new Node(6);
-let n8 = new Node(5);
-let n9 = new Node(2);
-let n10 = new Node(14);
-let n11 = new Node(1);
+let n4 = new Node(8);
+let n5 = new Node(12);
+let n6 = new Node(17);
+let n7 = new Node(25);
 
 
-tree.root = n1;
+searchTree.root = n1;
 n1.left = n2;
 n1.right = n3;
 n2.left = n4;
 n2.right = n5;
 n3.left = n6;
 n3.right = n7;
-n4.left = n8;
-n4.right = n9;
-n5.left = n10;
-n5.right = n11;
 
 let original = [];
 //iterate over tree and print nodes
@@ -47,7 +39,7 @@ function treePrint(root) {
   }
 }
 
-treePrint(tree.root);
+// treePrint(searchTree.root);
 
 let inverted = [];
 
@@ -61,7 +53,27 @@ function invertTree(root) {
   return root;
 };
 
-invertTree(tree.root)
+// invertTree(searchTree.root)
 
-console.log("ORIGINAL", original, "INVERTED", inverted);
+//search method for binary search tree
+function search(number, root) {
+  console.log("NUMBER", number, "ROOT", root.val)
+  if (!root) return;
+  if (root.val === number) {
+    console.log("FOUND", root);
+    return root;
+  }
+    
+    if (number < root.right.val) {
+      search(number, root.left);
+    } else {
+      search(number, root.right);
+    }
+}
+
+search(25, searchTree.root);
+
+search()
+
+// console.log("ORIGINAL", original, "INVERTED", inverted);
 
